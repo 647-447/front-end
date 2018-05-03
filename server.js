fffa85070c96320f3cdb160540f681b99e9a4c51
Nuno Neves <youknownuno@gmail.com>
2018-05-03 13:18:36 -0700

    pulled server.js out of the src folder

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express();
 
-app.use('/', express.static('../home'));
+app.use('/', express.static('./home'));
 
-app.use('/map', express.static('../map'));
+app.use('/map', express.static('./map'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
